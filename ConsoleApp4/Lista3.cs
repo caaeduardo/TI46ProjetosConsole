1fcee28a3cef19ad5ea88615b1bfc000d62f24e6
Giovanne Silvério Marcondes <giovanne.smarcondes@senacsp.edu.br>
2024-10-14 20:38:12 -0300

    Exrcício 4 listas

---
+++ ConsoleApp4/Lista3.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetosConsole
+{
+    internal class Lista3
+    {
+        internal void Exe1()
+        {
+            Console.WriteLine("1.Faça um programa que receba 3 valores e retorne a soma deles.\n");
+            {
+
+            }
+            Console.WriteLine("Fim do exercício 1");
+            Console.WriteLine("-------------------------------------------------------------");
+        }
+
+
+        internal void Exe2()
+        {
+            Console.WriteLine("2.Faça um programa que receba quatro notas e retorne a média.\n");
+            {
+
+            }
+            Console.WriteLine("Fim do exercício 2");
+            Console.WriteLine("-------------------------------------------------------------");
+        }
+
+        internal void Exe3()
+        {
+            Console.WriteLine("3.Faça um programa que receba um nome enquanto o nome for diferente de João continuar pedindo o nome\n");
+            {
+
+            }
+            Console.WriteLine("Fim do exercício 3");
+            Console.WriteLine("-------------------------------------------------------------");
+        }
+        internal void Exe4()
+        {
+            Console.WriteLine("4.\tFaça um programa que retorne à quantidade de números pares existente entre 35 e 98111");
+            {
+
+            }
+            Console.WriteLine("Fim do exercício 4\n");
+            Console.WriteLine("-------------------------------------------------------------");
+        }
+
+
+        internal void Exe5()
+        {
+            Console.WriteLine("5.\tFaÇa um programa que some todos os números ímpares de 1 até 5000\n");
+            {
+
+            }
+            Console.WriteLine("Fim do exercício 5");
+            Console.WriteLine("-------------------------------------------------------------");
+        }
+
+        internal void Exe6()
+        {
+            Console.WriteLine("6.Faça um programa que receba um número e usando laços de repetição calcule e mostre a tabuada desse número.\n");
+            {
+
+            }
+            Console.WriteLine("Fim do exercício 6");
+            Console.WriteLine("-------------------------------------------------------------");
+        }
+
+        internal void Exe7()
+        {
+            Console.WriteLine("6.Faça um programa que receba um número e usando laços de repetição calcule e mostre a tabuada desse número.\n");
+            {
+
+            }
+            Console.WriteLine("Fim do exercício 7");
+            Console.WriteLine("-------------------------------------------------------------");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
